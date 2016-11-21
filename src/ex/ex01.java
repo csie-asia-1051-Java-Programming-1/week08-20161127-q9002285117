797f4e1c8c9db0826a8eaa7a3440f750157826f0
q9002285117 <105021038@live.asia.edu.tw>
2016-11-21 16:13:05 +0800

    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,15 +1,28 @@
 ﻿package ex;
+
 /*
  * Topic: 請寫一個函式 (不是遞迴) 進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021038 傅琬鈞
  */
+import java.util.Scanner;
 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println(fun(n));
 	}
 
+	public static String fun(int a) {
+		String i = "";
+		while (a > 0) {
+			i = Integer.toBinaryString(a % 2) + i;
+			a = a / 2;
+
+		}
+		return i;
+	}
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,26 @@
 ﻿package ex;
+
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021038 傅琬鈞
  */
+import java.util.Scanner;
 
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int m = scn.nextInt();
+		String i = "";
+		while (n > 0 && m > 0) {
 
+			i = (n % m) + i;
+			n = n / m;
+		}
+		System.out.print(i);
 	}
 
 }
